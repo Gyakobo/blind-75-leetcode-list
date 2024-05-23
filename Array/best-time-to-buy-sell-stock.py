5dd778e04f5c3832474309b16b23d8f7c0109b1e
Gyakobo <andygyakobo@gmail.com>
2024-05-23 16:52:22 -0400

    Finished writing best-time-to-busy-sell-stock

--- Array/best-time-to-buy-sell-stock.py
+++ Array/best-time-to-buy-sell-stock.py
@@ -1,3 +1,19 @@
 class Solution:
-    pass
+    def maxProfit(self, prices) -> int:
+        l, r = 0, 1 # left=buy, right=sell
+        maxP = 0
 
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit  = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            else:
+                l += 1
+            r += 1
+
+        return maxP
+
+prices = [7, 1, 5, 3, 6, 4]
+
+s = Solution()
+print(s.maxProfit(prices))
