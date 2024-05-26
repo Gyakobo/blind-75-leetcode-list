89c3d70d108565f6969303de3e7ac8a980addfde
Gyakobo <andygyakobo@gmail.com>
2024-05-26 17:48:15 -0400

    Finished doing product-of-array-except-self

--- Array/product-of-array-except-self.py
+++ Array/product-of-array-except-self.py
@@ -1,7 +1,22 @@
 class Solution:
-    def solution(self):
-        pass
+    def solution(self, nums):
+        n = len(nums)
+        res = [1] * n
+
+        pre = post = 1
+
+        res[0] = pre
+        for i in range(n-1):
+            pre *= nums[i]
+            res[i+1] = pre
+
+        for i in range(n-1, 0, -1):
+            post *= nums[i]
+            res[i-1] *= post
+
+        return res 
+
+lst = [1, 2, 3, 4]
 
 s = Solution()
-
-[1, 2, 3, 4]
+print(s.solution(lst))
