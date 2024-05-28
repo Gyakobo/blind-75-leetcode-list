21e277770aa7dd696747e49c3a9a4ca6642c40ad
Gyakobo <andygyakobo@gmail.com>
2024-05-27 21:27:54 -0400

    Finished writing maximum-product-subarray

--- Array/maximum-product-subarray.py
+++ Array/maximum-product-subarray.py
@@ -1,7 +1,20 @@
 class Solution:
     def solution(self, nums):
-        pass
+        res = max(nums) # 0
+        curMin, curMax = 1, 1
 
+        for n in nums:
+            if n == 0:
+                curMin, curMax = 1, 1
+                continue
+
+            tmp = curMax * n
+            curMax = max(n * curMax, n * curMin, n)
+            curMin = min(tmp, n * curMin, n)
+            res = max(res, curMax)
+        
+        return res
+
+s = Solution()
 lst = [2, 3, -2, 4]
-s = solution()
 print(s.solution(lst))
