7119b9a8ad0f2002ca99d98746ef06a8f4249275
Gyakobo <andygyakobo@gmail.com>
2024-05-29 15:04:18 -0400

    Finished doing rotate-image

--- Matrix/rotate-image.py
+++ Matrix/rotate-image.py
@@ -1,11 +1,19 @@
 class Solution:
     def solution(self, mtx):
+        m = len(mtx)
+        n = len(mtx[0])
 
-        return 
+        mtx.reverse() 
+
+        for c in range(m):
+            for r in range(c):
+                mtx[r][c], mtx[c][r] = mtx[c][r], mtx[r][c] 
+
+        return mtx
 
 matrix = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]
 
 # Desired output: [ [7, 4, 1], [8, 5, 2], [9, 6, 3] ]
 
 s = Solution()
-print(s.solution())
+print(s.solution(matrix))
