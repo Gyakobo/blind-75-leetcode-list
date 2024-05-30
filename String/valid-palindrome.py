b3199220cb275f0ae445f05f18e0151656769e3c
Gyakobo <andygyakobo@gmail.com>
2024-05-30 15:01:40 -0400

    Attempting Valid-Palindrome

---
+++ String/valid-palindrome.py
@@ -0,0 +1,13 @@
+class Solution:
+    def solution(self, word):
+        if not word: return 
+
+        n = len(word)
+
+        if n % 2 == 0:
+            for c in word:
+            
+
+lst = []
+s = Solution()
+print(s.solution(lst))
