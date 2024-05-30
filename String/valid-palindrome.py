b0e22a6e36cb73756eb029140d5d4c1e1ea29512
Gyakobo <andygyakobo@gmail.com>
2024-05-30 15:13:31 -0400

    Finished solving valid-palindrome

--- String/valid-palindrome.py
+++ String/valid-palindrome.py
@@ -1,13 +1,15 @@
 class Solution:
+    # Time Complexity: O(n/2)
     def solution(self, word):
         if not word: return 
 
         n = len(word)
 
-        if n % 2 == 0:
-            for c in word:
+        for i in range(n//2):
+            if word[i] != word[n-1-i]:
+                return False
             
+        return True 
 
-lst = []
 s = Solution()
-print(s.solution(lst))
+print(s.solution("abba"))
