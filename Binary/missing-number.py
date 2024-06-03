8723c42bfd5bbe26f3a06d32c1f4929cf7b566df
Gyakobo <andygyakobo@gmail.com>
2024-06-03 00:44:21 -0400

    Test commit

--- Binary/missing-number.py
+++ Binary/missing-number.py
@@ -2,9 +2,11 @@ class Solution:
     def missing_number(self, nums):
         n = len(nums)
         s = sum(nums)
-        
+        e = (n * (n+1)) // 2
+
+        return e - n         
 
 nums = [3, 0, 1]
 
 s = Solution()
-print(s.missing_number())
+print(s.missing_number(nums))
