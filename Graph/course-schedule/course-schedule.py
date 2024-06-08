7abcd0fc5877e76bbb9efa254bd5e3e55e9231a1
Gyakobo <andygyakobo@gmail.com>
2024-06-07 20:57:04 -0400

    Still working on the course-scheduler

--- Graph/course-schedule/course-schedule.py
+++ Graph/course-schedule/course-schedule.py
@@ -1,19 +1,22 @@
 class Solution:
-    def course_schedule(self, courses):
+    def course_schedule(self, n, courses):
         # Create a dependency graph
-        graph = { i[0]:i[1] for i in courses }
+        graph = {int:[]}
+        
+        graph = { i: for i in range(n)  }
 
-        passed = set()
+        visited = set()
 
         def dfs(c):
-            if c in passed:
+            if c in visited:
                 return False
 
-            
+
 
         print(graph)
 
+n = 6 
 courses = [ [4, 5], [3, 4], [2, 3], [1, 2], [0, 1] ]
 s = Solution()
-print(s.course_schedule(courses)) 
+print(s.course_schedule(n, courses)) 
 
