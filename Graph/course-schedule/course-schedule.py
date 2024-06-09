15cf826ce5b48b931b785f2e846b0a523aa92ed1
Gyakobo <andygyakobo@gmail.com>
2024-06-09 14:38:26 -0400

    I technically solved this before but I honestly don't like the solution of course-scheduler

--- Graph/course-schedule/course-schedule.py
+++ Graph/course-schedule/course-schedule.py
@@ -15,7 +15,23 @@ class Solution:
         4: [] 
         '''
 
-        # visited = set()
+        visited = set()
+        def dfs(crs):
+            if crs in visited:
+                return False
+            if preMap[crs] == []:
+                return True
+
+            visited.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre): return False
+            visited.remove(crs)
+            preMap[crs] = []
+            return True
+        
+        for crs in range(numCourses):
+            if not dfs(crs): return False
+        return True
 
         print(graph)
  
