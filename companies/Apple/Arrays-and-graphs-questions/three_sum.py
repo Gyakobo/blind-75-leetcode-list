93f9e8faff2402c4e24c4524a2f66a05c011244a
Gyakobo <andygyakobo@gmail.com>
2025-01-31 16:05:43 -0500

    Trying to solve Apple's three-sum

--- companies/Apple/Arrays-and-graphs-questions/three_sum.py
+++ companies/Apple/Arrays-and-graphs-questions/three_sum.py
@@ -1,6 +1,37 @@
 target = 20
 
 def three_sum(arr, target = target):
-    pass
+    n = len(arr)
+    res = [] 
 
+    # Pointers to indices  
+    i = 0
+    low = i+1 
+    high = n-1 
+
+    # Main Operation
+    arr.sort() # nlogn 
+    print("Array:", arr)
+
+    for i in range(n-2):
+        while low != high:
+            total = arr[i] + arr[low] + arr[high]
+            print(i, total)
+
+            if total == target:
+                res.append([i, low, high])
+            elif total < target:
+                low += 1
+            elif total > target:
+                high -= 1
+            elif low == high:
+                low = i + 1
+                high = n - 1 
+                # break 
+        
+    return res
+
+#       i     l->       <-h
 nums = [3, 7, 1, 2, 8, 4, 5]
+
+print(three_sum(nums))
--- companies/Apple/Arrays-and-graphs-questions/two_sum.py
+++ companies/Apple/Arrays-and-graphs-questions/two_sum.py
@@ -1,16 +1,22 @@
 target = 5
 
+# 2 + 3 = 5
 
-# 2 + 3 = 5
-# 2 + 3 = 5
+# i  (t   j)
+# 2 = 5 - 3
+
+# dct = {}
 
 def two_sum(arr, target = target):
     dct = {int:int}
 
     for (key, value) in enumerate(arr):
-
-
+        if value in dct:
+            print(dct[value], key)
         else:
+            dct[target - value] = key
+    return 
 
-
-nums = [2, 3, 4, 8]
+#       0  1  2  3
+nums = [2, 4, 3, 8]
+two_sum(nums)
