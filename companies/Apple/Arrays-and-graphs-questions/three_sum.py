95ed536c38d331edeaa9871dfdfa56d255ca6637
Gyakobo <andygyakobo@gmail.com>
2025-01-31 13:18:42 -0500

    Started going through a new company's leetcode questions: Apple

---
+++ companies/Apple/Arrays-and-graphs-questions/three_sum.py
@@ -0,0 +1,6 @@
+target = 20
+
+def three_sum(arr, target = target):
+    pass
+
+nums = [3, 7, 1, 2, 8, 4, 5]
---
+++ companies/Apple/Arrays-and-graphs-questions/two_sum.py
@@ -0,0 +1,16 @@
+target = 5
+
+
+# 2 + 3 = 5
+# 2 + 3 = 5
+
+def two_sum(arr, target = target):
+    dct = {int:int}
+
+    for (key, value) in enumerate(arr):
+
+
+        else:
+
+
+nums = [2, 3, 4, 8]
