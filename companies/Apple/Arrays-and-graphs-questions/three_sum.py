fcfec307227df6b2ee26ac16139c4538fb6fc004
Gyakobo <andygyakobo@gmail.com>
2025-01-31 16:19:46 -0500

    Finally solve Apple's Three Sum problem

--- companies/Apple/Arrays-and-graphs-questions/three_sum.py
+++ companies/Apple/Arrays-and-graphs-questions/three_sum.py
@@ -14,24 +14,24 @@ def three_sum(arr, target = target):
     print("Array:", arr)
 
     for i in range(n-2):
-        while low != high:
+        low = i + 1
+        high = n - 1 
+        
+        while low < high:
             total = arr[i] + arr[low] + arr[high]
-            print(i, total)
+            # print(i, total)
 
             if total == target:
-                res.append([i, low, high])
+                res.append([arr[i], arr[low], arr[high]])
+                break
             elif total < target:
                 low += 1
             elif total > target:
                 high -= 1
-            elif low == high:
-                low = i + 1
-                high = n - 1 
-                # break 
         
     return res
 
 #       i     l->       <-h
-nums = [3, 7, 1, 2, 8, 4, 5]
+nums = [3, 7, 1, 2, 8, 4, 5, 10, 10, 15, 5]
 
 print(three_sum(nums))
