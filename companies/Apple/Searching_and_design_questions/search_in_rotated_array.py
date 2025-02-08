245cbd66a12aee9321affc57f1d98f6dbb51e815
Gyakobo <andygyakobo@gmail.com>
2025-02-07 19:58:41 -0500

    Fixed the 'search-in-rotated-array' problem

--- companies/Apple/Searching_and_design_questions/search_in_rotated_array.py
+++ companies/Apple/Searching_and_design_questions/search_in_rotated_array.py
@@ -1,21 +1,37 @@
-def binary_search_rotated(nums:list[int], key:int) -> int:
-    n = len(nums)
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        
+        min_index = 0
+        middle = 0
+        left = 0
+        right = n-1
 
-    min_index = 0
-    middle = 0
-    left = 0
-    right = 0
+        while left < right:
+            middle = (left + right) // 2
+            
+            if nums[middle] > nums[right]:
+                left = middle + 1
+            else:
+                right = middle
 
-    while left < right:
-        middle = (left + right) // 2
+        min_index = left
 
-        if nums[middle] > nums[right]:
-            left = middle + 1
+        if min_index == 0:
+            left, right = 0, n-1
+        elif nums[0] <= target and target <= nums[min_index-1]:
+            left, right = 0, min_index-1
         else:
-            right = middle
+            left, right = min_index, n-1
 
-    min_index = left
+        while left <= right:
+            middle = (left + right) // 2
 
-    return min_index
+            if nums[middle] == target:
+                return middle
+            elif target > nums[middle]:
+                left = middle + 1
+            else:
+                right = middle - 1
 
-
+        return -1
